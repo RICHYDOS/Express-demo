e994db6a1e7c2a74b656f512b33a19c18e62a120
Richard <richardosunmu@gmail.com>
2022-12-26 18:31:57 +0100

    Redoing what I learned here

--- index.js
+++ index.js
@@ -1,74 +1,29 @@
-const Joi = require("joi");
 const express = require('express');
 const app = express();
-app.use(express.json());
-
-const accounts = [
-    {id: 1, username:"Richard"},
-    {id: 2, username:"Feranmi"},
-    {id: 3, username:"Dosunmu"},
+const courses = [
+    { id: 1, name: "Course1" },
+    { id: 2, name: "Course2" },
+    { id: 3, name: "Course3" },
 ]
 
-app.get('/api', (req, res) => {
-    res.send("Hello World")
+
+app.get('/', (req, res) => {
+    res.send("Hello World!!!");
 });
 
 app.get('/api/courses', (req, res) => {
-    res.send(accounts);
-})
+    res.send(courses);
+});
 
+
+// An API endpoint for getting a particular course
 app.get('/api/courses/:id', (req, res) => {
-    const account = accounts.find(c => c.id === parseInt(req.params.id));
-    if (!account){
-        res.status(404).send("No Username at this index");
-    }
-    res.send(account);
-})
+    // The "find" method takes in an array object and checks whether the id in each object is the same as the id request parameter
+    const course = courses.find(c => c.id === parseInt(req.params.id));
+    if (!course)
+        res.status(404).send("Course not found");
+    res.send(course)
+});
 
-
-app.post('/api/courses', (req, res) => {
-
-    const {error} = schema.validateAccount(req.body);
-    if (error){
-        res.status(404).send(error.details[0].message);
-        return
-    }
-
-    const account = {
-        id: accounts.length + 1,
-        name: req.body.username
-    }
-    accounts.push(account);
-    res.send(account);
-})
-
-
-app.put('/api/courses/:id', (req, res) => {
-    const account = accounts.find(c => c.id === parseInt(req.params.id));
-    if (!account){
-        res.status(404).send("No Username at this index");
-    }
-
-    const {error} = schema.validateAccount(req.body);
-    if (error){
-        res.status(404).send(error.details[0].message);
-        return
-    }
-    account.username = req.body.username;
-    return res.send(account)
-
-})
-
-
-const port = process.env.PORT || 3000;
-
-app.listen(port, () => console.log(`Port ${port} Working`));
-
-function validateAccount(accountval){
-
-    const schema = Joi.object({
-    username: Joi.string().min(3).required()
-    })
-
-    return schema.validate(accountval);
-}
+const port = process.env.PORT || 3000
+app.listen(port, () => console.log(`Listening on Port ${port}`));
