3aef8c8313cd529fd326b6e652752d3f60810dbd
Richard <richardosunmu@gmail.com>
2022-12-27 22:04:09 +0100

    Built The Vidly API
    A very simple API for viewing, creating, updating and deleting Movie Genres

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const Joi = require('joi');
 const express = require('express');
 const app = express();
 app.use(express.json());
@@ -24,11 +25,15 @@ app.get('/api/courses/:id', (req, res) => {
     const course = courses.find(c => c.id === parseInt(req.params.id));
     if (!course)
         res.status(404).send("Course not found");
-    res.send(course)
+    res.send(course);
 });
 
 app.post('/api/courses', (req, res) => {
-    console.log(req.body, courses);
+    // Using the Joi class for Input Validation
+    const result = validateCourse(req.body);
+    if (result.error)
+        return res.status(400).send(result.error.details[0].message);
+
     const course = {
         id: courses.length + 1,
         name: req.body.name,
@@ -36,6 +41,39 @@ app.post('/api/courses', (req, res) => {
     courses.push(course);
     res.send(course);
 });
+
+app.put('/api/courses/:id', (req, res) => {
+    // Check if the particular course is in our list of courses
+    const course = courses.find(c => c.id === parseInt(req.params.id));
+    if (!course)
+        return res.status(404).send("Course not found");
+
+    //validate user input
+    const result = validateCourse(req.body);
+    if (result.error)
+        return res.status(400).send(result.error.details[0].message);
+
+    course.name = req.body.name;
+    res.send(course);
+});
+
+app.delete('/api/courses/:id', (req, res) => {
+    const course = courses.find(c => c.id === parseInt(req.params.id));
+    if (!course)
+        return res.status(404).send("Course not found");
+
+    const index = courses.indexOf(course);
+    courses.splice(index, 1);
+    res.send(course);
+})
+
+function validateCourse(course){
+    const schema = Joi.object({
+        name: Joi.string().min(3).required(),
+    });
+
+    return schema.validate(course);
+}
 
 const port = process.env.PORT || 3000
 app.listen(port, () => console.log(`Listening on Port ${port}`));
